0392727345f9a807c459851679e9f05ad79fd007
rbrianshutt <rbrianshutt@gmai.com>
2022-11-26 16:35:59 -0600

    correct error

--- pages/pokemon.jsx
+++ pages/pokemon.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Layout from '../components/layout';
+import Layout from '../components/Layout.jsx';
 import Link from 'next/link';
 
 function pokemon({ pokeman }) {
